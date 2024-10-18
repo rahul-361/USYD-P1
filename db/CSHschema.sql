8c307b015729e031de53ea166d45a93264133bc7
allenalvin333 <allenalvin333@gmail.com>
2024-10-19 10:28:51 +1100

    SQL

--- db/CSHschema.sql
+++ db/CSHschema.sql
@@ -1,80 +1,80 @@
-DROP TABLE IF EXISTS Administrator;
-DROP TABLE IF EXISTS Patient;
-DROP TABLE IF EXISTS AdmissionType;
-DROP TABLE IF EXISTS Department;
-DROP TABLE IF EXISTS Admission;
+DROP TABLE IF EXISTS ADMINISTRATOR;
+DROP TABLE IF EXISTS PATIENT;
+DROP TABLE IF EXISTS ADMISSIONTYPE;
+DROP TABLE IF EXISTS DEPARTMENT;
+DROP TABLE IF EXISTS ADMISSION;
 
-CREATE TABLE Administrator (
-    UserName VARCHAR(10) PRIMARY KEY,
-    Password VARCHAR(20) NOT NULL,
-    FirstName VARCHAR(50) NOT NULL,
-    LastName VARCHAR(50) NOT NULL,
-    Email VARCHAR(20) NOT NULL
+CREATE TABLE ADMINISTRATOR (
+    USERNAME VARCHAR(10) PRIMARY KEY,
+    PASSWORD VARCHAR(20) NOT NULL,
+    FIRSTNAME VARCHAR(50) NOT NULL,
+    LASTNAME VARCHAR(50) NOT NULL,
+    EMAIL VARCHAR(20) NOT NULL
 );
 
-INSERT INTO Administrator VALUES 
-('jdoe', 'Pass1234', 'John', 'Doe', 'jdoe@csh.com'),
-('jsmith', 'Pass5678', 'Jane', 'Smith', 'jsmith@csh.com'),
-('ajohnson', 'Passabcd', 'Alice', 'Johnson', 'ajohnson@csh.com'),
-('bbrown', 'Passwxyz', 'Bob', 'Brown', 'bbrown@csh.com'),
-('cdavis', 'Pass9876', 'Charlie', 'Davis', 'cdavis@csh.com'),
-('ksmith', 'Pass5566', 'Karen', 'Smith', 'ksmith@csh.com');
+INSERT INTO ADMINISTRATOR VALUES 
+    ('jdoe', 'Pass1234', 'John', 'Doe', 'jdoe@csh.com'),
+    ('jsmith', 'Pass5678', 'Jane', 'Smith', 'jsmith@csh.com'),
+    ('ajohnson', 'Passabcd', 'Alice', 'Johnson', 'ajohnson@csh.com'),
+    ('bbrown', 'Passwxyz', 'Bob', 'Brown', 'bbrown@csh.com'),
+    ('cdavis', 'Pass9876', 'Charlie', 'Davis', 'cdavis@csh.com'),
+    ('ksmith', 'Pass5566', 'Karen', 'Smith', 'ksmith@csh.com');
 
-CREATE TABLE Patient (
-    PatientID VARCHAR(10) PRIMARY KEY,
-    Password VARCHAR(20) NOT NULL,
-    FirstName VARCHAR(50) NOT NULL,
-    LastName VARCHAR(50) NOT NULL,
-    Mobile VARCHAR(20) NOT NULL
+CREATE TABLE PATIENT (
+    PATIENTID VARCHAR(10) PRIMARY KEY,
+    PASSWORD VARCHAR(20) NOT NULL,
+    FIRSTNAME VARCHAR(50) NOT NULL,
+    LASTNAME VARCHAR(50) NOT NULL,
+    MOBILE VARCHAR(20) NOT NULL
 );
 
-INSERT INTO Patient VALUES 
-('dwilson', 'Pass5432', 'David', 'Wilson', '4455667788'),
-('etylor', 'Passlmno', 'Eva', 'Taylor', '5566778899'),
-('faderson', 'Passrstu', 'Frank', 'Anderson', '6677889900'),
-('gthomas', 'Pass1357', 'Grace', 'Thomas', '7788990011'),
-('smartinez', 'Pass2468', 'Stan', 'Martinez', '8899001122'),
-('lroberts', 'Pass1122', 'Laura', 'Roberts', '9900112233');
+INSERT INTO PATIENT VALUES 
+    ('dwilson', 'Pass5432', 'David', 'Wilson', '4455667788'),
+    ('etylor', 'Passlmno', 'Eva', 'Taylor', '5566778899'),
+    ('faderson', 'Passrstu', 'Frank', 'Anderson', '6677889900'),
+    ('gthomas', 'Pass1357', 'Grace', 'Thomas', '7788990011'),
+    ('smartinez', 'Pass2468', 'Stan', 'Martinez', '8899001122'),
+    ('lroberts', 'Pass1122', 'Laura', 'Roberts', '9900112233');
 
 
-CREATE TABLE AdmissionType (
-    AdmissionTypeID SERIAL PRIMARY KEY,
-    AdmissionTypeName VARCHAR(20) UNIQUE NOT NULL
+CREATE TABLE ADMISSIONTYPE (
+    ADMISSIONTYPEID SERIAL PRIMARY KEY,
+    ADMISSIONTYPENAME VARCHAR(20) UNIQUE NOT NULL
 );
 
-INSERT INTO AdmissionType VALUES (1, 'Emergency');
-INSERT INTO AdmissionType VALUES (2, 'Transfer');
-INSERT INTO AdmissionType VALUES (3, 'Inpatient');
-INSERT INTO AdmissionType VALUES (4, 'Outpatient');
+INSERT INTO ADMISSIONTYPE VALUES (1, 'Emergency');
+INSERT INTO ADMISSIONTYPE VALUES (2, 'Transfer');
+INSERT INTO ADMISSIONTYPE VALUES (3, 'Inpatient');
+INSERT INTO ADMISSIONTYPE VALUES (4, 'Outpatient');
 
-CREATE TABLE Department (
-    DeptId SERIAL PRIMARY Key,
-    DeptName VARCHAR(20) UNIQUE not NULL
+CREATE TABLE DEPARTMENT (
+    DEPTID SERIAL PRIMARY KEY,
+    DEPTNAME VARCHAR(20) UNIQUE NOT NULL
 );
 
-INSERT INTO Department VALUES (1, 'General');
-INSERT INTO Department VALUES (2, 'Emergency');
-INSERT INTO Department VALUES (3, 'Surgery');
-INSERT INTO Department VALUES (4, 'Obstetrics');
-INSERT INTO Department VALUES (5, 'Rehabilitation');
-INSERT INTO Department VALUES (6, 'Paediatrics');
+INSERT INTO DEPARTMENT VALUES (1, 'General');
+INSERT INTO DEPARTMENT VALUES (2, 'Emergency');
+INSERT INTO DEPARTMENT VALUES (3, 'Surgery');
+INSERT INTO DEPARTMENT VALUES (4, 'Obstetrics');
+INSERT INTO DEPARTMENT VALUES (5, 'Rehabilitation');
+INSERT INTO DEPARTMENT VALUES (6, 'Paediatrics');
 
-CREATE table Admission (
-    AdmissionID SERIAL PRIMARY KEY,
-    AdmissionType INTEGER NOT NULL,
-    Department INTEGER NOT NULL,
-	Patient VARCHAR(10) NOT NULL,
-	Administrator VARCHAR(10) NOT NULL,
-    Fee Decimal(7,2),
-    DischargeDate Date,
-    Condition VARCHAR(500),
-	FOREIGN KEY(AdmissionType) REFERENCES AdmissionType,
-	FOREIGN KEY(Department) REFERENCES Department,
-	FOREIGN KEY(Patient) REFERENCES Patient,
-	FOREIGN KEY(Administrator) REFERENCES Administrator
+CREATE TABLE ADMISSION (
+    ADMISSIONID SERIAL PRIMARY KEY,
+    ADMISSIONTYPE INTEGER NOT NULL,
+    DEPARTMENT INTEGER NOT NULL,
+	PATIENT VARCHAR(10) NOT NULL,
+	ADMINISTRATOR VARCHAR(10) NOT NULL,
+    FEE DECIMAL(7,2),
+    DISCHARGEDATE DATE,
+    CONDITION VARCHAR(500),
+	FOREIGN KEY(ADMISSIONTYPE) REFERENCES ADMISSIONTYPE,
+	FOREIGN KEY(DEPARTMENT) REFERENCES DEPARTMENT,
+	FOREIGN KEY(PATIENT) REFERENCES PATIENT,
+	FOREIGN KEY(ADMINISTRATOR) REFERENCES ADMINISTRATOR
 );
 
-INSERT INTO Admission (AdmissionType, Department, Fee, Patient, Administrator, DischargeDate, Condition) VALUES
+INSERT INTO ADMISSION (ADMISSIONTYPE, DEPARTMENT, FEE, PATIENT, ADMINISTRATOR, DISCHARGEDATE, CONDITION) VALUES
     (4, 1, 666.00, 'lroberts', 'jdoe', '28/02/2024', 'a red patch on my skin that looks irritated. It started small but has been spreading and feels warm to the touch'),
 	(2, 1, 100.00, 'gthomas', 'jdoe', '11/09/2021', NULL),
 	(1, 2, NULL, 'lroberts','jsmith', '02/09/2019', 'Admitted to the emergency department after suffering head trauma from a fall, requiring a CT scan and observation for potential concussion.'),
